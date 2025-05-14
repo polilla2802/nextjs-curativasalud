6d74ce5c96c1f06ae0f73bc30590dab37e41d34e
Webmaster Tachuela <78869984+polilla2802@users.noreply.github.com>
2025-05-14 11:18:09 -0500

    added supabase + storage

---
+++ src/lib/supabaseConfig.js
@@ -0,0 +1,7 @@
+// src/lib/supabaseConfig.js
+const supabaseConfig = {
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+};
+
+export default supabaseConfig;
