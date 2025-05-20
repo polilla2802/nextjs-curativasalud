2bf5448a5ed9869dfd6db2bc387babc35ccdbe1b
Webmaster Tachuela <78869984+polilla2802@users.noreply.github.com>
2025-05-20 09:05:26 -0500

    Coming soon

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,9 @@ export default function Home() {
   return (
     <>
       <section className="container px-0 py-4 m-auto md:px-4">
-        <h1 className="text-4xl font-bold text-center text-white md:text-8xl">Tarjetas Miembros</h1>
-        <ContentForm />
+        <h1 className="pt-5 text-4xl font-bold text-center text-white md:text-8xl">Proximamente</h1>
+        {/* <ContentForm /> */}
+        <img src="/images/logo-CUSACAN-HEADER.png" alt="CUSACAN" className="px-10 pt-10 m-auto" />
       </section>
     </>
   );
