5a2fce6c6e518a7a99b5b0f0d52b1e6f1a72b38a
Webmaster Tachuela <78869984+polilla2802@users.noreply.github.com>
2025-05-20 09:54:50 -0500

    files name changes to lowercase

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="container px-0 py-4 m-auto md:px-4">
         <h1 className="pt-5 text-4xl font-bold text-center text-white md:text-8xl">Proximamente</h1>
         {/* <ContentForm /> */}
-        <img src="/images/logo-CUSACAN-HEADER.png" alt="CUSACAN" className="px-10 pt-10 m-auto" />
+        <img src="/images/logo-cusacan.png" alt="CUSACAN" className="px-10 pt-10 m-auto" />
       </section>
     </>
   );
